21766959ea01455633508087b8c0917df95703cc
shikanothuno <ri0066xr@gmail.com>
2024-06-19 15:50:59 +0900

    fix shop-detail

--- src/resources/views/shop-detail.blade.php
+++ src/resources/views/shop-detail.blade.php
@@ -12,7 +12,7 @@
 <main>
     <div class="shop-detail">
         <div class="shop-detail-header">
-            <a href="{{ route("shop-list") }}">&lt</a>
+            <a class="back-button" href="{{ route("shop-list") }}">&lt</a>
             <h1 class="shop-name">{{ $shop->name }}</h1>
         </div>
         <div class="shop-img__div">
@@ -20,49 +20,49 @@
         </div>
         <div class="shop-region-and-genre">
             {{ "#" . $shop->region . " " . "#" . $shop->genre }}</div>
-        <div class="shop-descrtion">{{ $shop->descriton }}</div>
+        <div class="shop-description">{{ $shop->description }}</div>
     </div>
     <div class="reservation">
-        <form action="{{ route("store",$shop->id) }}" method="POST">
+        <div class="title">予約</div>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+        <form class="reservation-form" id="reservation-form" action="{{ route("store",$shop->id) }}" method="POST">
             @csrf
-            <label for="date">日付:</label>
-            <input type="date" id="date" name="date" required>
+            <input class="date" type="date" id="date" name="date" value="{{ old("date") }}" required>
             <br>
-            <label for="time">時間:</label>
-            <input type="time" id="time" name="time" required>
+            <input class="time" type="time" id="time" name="time" value="{{ old("time") }}" required>
             <br>
-            <label for="people">人数:</label>
-            <select id="people" name="number_of_people_booked">
+            <select class="people" id="people" name="number_of_people_booked">
+            </form>
                 @for ($i = 1; $i <= 20; $i++)
                     <option value={{ $i }}>{{ $i . "人" }}</option>
                 @endfor
             </select>
             <br>
             <div class="reservation-detail"></div>
-            <table>
-                <tr>
-                    <td>Shop</td>
-                    <td>{{ $shop->name }}</td>
-                </tr>
-                <tr>
-                    <td>Date</td>
-                    <td><span id="date-preview"></span></td>
-                </tr>
-                <tr>
-                    <td>Time</td>
-                    <td><span id="time-preview"></span></td>
-                </tr>
-                <tr>
-                    <td>Number</td>
-                    <td><span id="people-preview"></span></td>
-                </tr>
-            </table>
-            <button type="submit">予約する</button>
-        </form>
+                <table>
+                    <tr>
+                        <td>Shop</td>
+                        <td>{{ $shop->name }}</td>
+                    </tr>
+                    <tr>
+                        <td>Date</td>
+                        <td><span id="date-preview"></span></td>
+                    </tr>
+                    <tr>
+                        <td>Time</td>
+                        <td><span id="time-preview"></span></td>
+                    </tr>
+                    <tr>
+                        <td>Number</td>
+                        <td><span id="people-preview"></span></td>
+                    </tr>
+                </table>
+
+        <button class="submit-button" onclick="document.getElementById('reservation-form').submit()">予約する</button>
     </div>
 </main>
-@endsection
-
 <script>
     document.getElementById("date").addEventListener("input",function(){
         document.getElementById("date-preview").textContent = this.value;
@@ -75,4 +75,10 @@
     document.getElementById("people").addEventListener("input",function(){
         document.getElementById("people-preview").textContent = this.value;
     });
+
+    function submitForm(){
+        document.getElementById('reservation-form').submit();
+    };
 </script>
+@endsection
+
