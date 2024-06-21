4c4b4be3c8b7e0fd9d6f63ca8611a11986173b97
shikanothuno <ri0066xr@gmail.com>
2024-06-21 10:26:18 +0900

    fix search bar layout

--- src/resources/views/layouts/layout-base.blade.php
+++ src/resources/views/layouts/layout-base.blade.php
@@ -9,31 +9,36 @@
         @yield('css')
     </head>
     <body>
-        <div class="logo_div">
-            <h2 class="logo">Rese</h2>
+        <div class="header-white-space"></div>
+        <div class="header">
+            <div class="logo_div">
+                <h2 class="logo">Rese</h2>
+            </div>
+            <form method="POST" action="{{ route("logout") }}">
+                @csrf
+                <button class="logout-button">ログアウト</button>
+            </form>
+            @if(Route::currentRouteName() == "shop-list")
+            <div class=search_div>
+                <form class="search=form" id="search-form" action="{{ route("shop-list") }}"></form>
+                <select class="region__select" name="region" id="region" onchange="document.getElementById('search-form').submit()">
+                    <option value="">All area</option>
+                    @foreach ($regions as $region)
+                        <option value="{{ $region->id }}" {{ old('region', $select_region) == $region->name ? 'selected' : '' }}>{{ $region->name }}</option>
+                    @endforeach
+                </select>
+                <select class="genre__select" name="genre" id="genre" onchange="document.getElementById('search-form').submit()">
+                    <option value="">All genre</option>
+                    @foreach ($genres as $genre)
+                        <option value="{{ $genre->id }}" {{ old('genre', $select_genre) == $genre->name ? 'selected' : '' }}>{{ $genre->name }}</option>
+                    @endforeach
+                </select>
+                <img class="search-button" src="{{ asset("images/search.png") }}" alt="">
+                <input class="search__input" type="text" placeholder="Search ...">
+            </div>
+            @endif
         </div>
-        <form method="POST" action="{{ route("logout") }}">
-            @csrf
-            <button class="logout-button">ログアウト</button>
-        </form>
-        @if(Route::currentRouteName() == "shop-list")
-        <div class=search_div>
-            <form id="search-form" action="{{ route("shop-list") }}"></form>
-            <select name="region" id="region" onchange="document.getElementById('search-form').submit()">
-                <option value="">All area</option>
-                @foreach ($regions as $region)
-                    <option value="{{ $region->id }}" {{ old('region', $select_region) == $region->name ? 'selected' : '' }}>{{ $region->name }}</option>
-                @endforeach
-            </select>
-            <select name="genre" id="genre" onchange="document.getElementById('search-form').submit()">
-                <option value="">All genre</option>
-                @foreach ($genres as $genre)
-                    <option value="{{ $genre->id }}" {{ old('genre', $select_genre) == $genre->name ? 'selected' : '' }}>{{ $genre->name }}</option>
-                @endforeach
-            </select>
-            <input type="text" placeholder="Search ...">
-        </div>
-        @endif
+
 
         @yield('content')
     </body>
