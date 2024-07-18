57149b14e02a20868ac3b76c71eab5154520b64c
shikanothuno <ri0066xr@gmail.com>
2024-07-18 15:57:18 +0900

    fix web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -11,12 +11,7 @@ use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\ShopController;
 use App\Http\Controllers\StoreRepresentativeController;
 use App\Http\Controllers\UpdateShopInfoController;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/', [ShopController::class,"index"])->middleware(['auth', 'verified'])->name('dashboard');
 
