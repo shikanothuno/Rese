2799d1064dc82b561a4b5c7ab7cf1e2fcb029da4
shikanothuno <ri0066xr@gmail.com>
2024-07-18 15:29:24 +0900

    register stripe provider and aliase

--- src/config/app.php
+++ src/config/app.php
@@ -123,4 +123,14 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 
+    'providers' => [
+        // ...
+        App\Providers\StripeServiceProvider::class,
+    ],
+
+    'aliases' => [
+        // ...
+        'Stripe' => App\Facades\Stripe::class,
+    ],
+
 ];
